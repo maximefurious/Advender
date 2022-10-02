97eae7447902b0c457eac8fddee88e88a94535f7
maximefurious <93995791+maximefurious@users.noreply.github.com>
2022-10-02 14:13:02 +0200

    Update config.php

--- membre/config.php
+++ membre/config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', '217.182.207.90');
-define('DB_USERNAME', 'user2');
-define('DB_PASSWORD', 'Lyceepassword29');
-define('DB_NAME', 'DBuser2');
+define('DB_SERVER', '...');
+define('DB_USERNAME', '...');
+define('DB_PASSWORD', '...');
+define('DB_NAME', '...');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
